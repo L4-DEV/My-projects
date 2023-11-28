1db180e7df731d93958ca479fbf1ecba1fb5d601
Lucas Wendel <lucaswendelns@hotmail.com>
2023-11-27 21:48:54 -0300

    Atualizando nome de pasta, criando connection string e classe context (banco conectado), Create e Read prontos.

---
+++ WebApi/WebApi/Data/Context/FilmeContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.Data.Context
+{
+    public class FilmeContext: DbContext
+    {
+        public FilmeContext(DbContextOptions <FilmeContext>opts):base(opts)
+        {
+            
+        }
+
+        public  DbSet <Filme> Filme{ get; set; }
+    }
+}
